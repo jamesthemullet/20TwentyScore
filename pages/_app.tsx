d188ac6b180c0d6ab88e8a50190717eae48753d0
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-06-26 16:02:50 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
