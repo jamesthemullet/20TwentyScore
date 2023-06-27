b7a1fd44695417cbb9fed062e1c8ef389d8e80e7
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-06-27 21:04:00 +0100

    h1

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -59,7 +59,7 @@ const Post: React.FC<PostProps> = (props) => {
   return (
     <Layout>
       <div>
-        <h2>{title}</h2>
+        <h1>{title}</h1>
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown>{props.content}</ReactMarkdown>
         {!props.published && userHasValidSession && postBelongsToUser && (
