6bad2923e4258db817d737cc937187a01fb327d6
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-06-29 19:18:56 +0100

    add very basic scoreboard

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,50 +1,28 @@
 import React, { ReactNode } from 'react';
 import Header from './Header';
+import styled from 'styled-components';
 
 type Props = {
   children: ReactNode;
 };
 
-const Layout: React.FC<Props> = (props) => (
-  <div>
-    <Header />
-
-    <div className="layout">{props.children}</div>
-    <style jsx global>{`
-      html {
-        box-sizing: border-box;
-      }
-
-      *,
-      *:before,
-      *:after {
-        box-sizing: inherit;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        font-size: 16px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial,
-          sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-        background: rgba(0, 0, 0, 0.05);
-      }
-
-      input,
-      textarea {
-        font-size: 16px;
-      }
-
-      button {
-        cursor: pointer;
-      }
-    `}</style>
-    <style jsx>{`
-      .layout {
-        padding: 0 2rem;
-      }
-    `}</style>
-  </div>
-);
+const Layout: React.FC<Props> = (props) => {
+  console.log(5, props);
+  return (
+    <Page>
+      <Header />
+      <div className="layout">{props.children}</div>
+    </Page>
+  );
+};
 
 export default Layout;
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+`;
---
+++ components/Scoreboard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Scoreboard = () => {
+  return (
+    <>
+      <h3>Scoreboard</h3>
+      <ScoreboardLayout>
+        <div>
+          <div>Team 1</div>
+          <div>Runs - Wickets (Overs)</div>
+        </div>
+        <div>
+          <div>Team 2</div>
+          <div>Runs - Wickets (Overs)</div>
+        </div>
+      </ScoreboardLayout>
+    </>
+  );
+};
+
+export default Scoreboard;
+
+const ScoreboardLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 1rem;
+  gap: 5rem;
+
+  // div last of type
+  & > div:last-of-type {
+    text-align: right;
+  }
+`;
