a7ddad5694dba80759cf2a354adb521ed684f52d
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-07-28 16:27:12 +0100

    nav footer and icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Nav from '../components/nav';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Nav />
       <Component {...pageProps} />
     </SessionProvider>
   );
