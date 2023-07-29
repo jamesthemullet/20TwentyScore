cf89225770fc8aff4b2ed92cc22745f8125b18da
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-07-29 09:17:32 +0100

    scoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Layout from '../components/Layout';
 import Scoreboard from '../components/Scoreboard';
@@ -6,6 +6,8 @@ import Post, { PostProps } from '../components/Post';
 import prisma from '../lib/prisma';
 import Team from '../components/Team';
 import styled from 'styled-components';
+import Scoring from '../components/scoring';
+import defaultPlayers from '../components/players';
 
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
@@ -22,19 +24,57 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
+type Player = {
+  name: string;
+  runs: number;
+  isBatting: boolean;
+  allActions: string[];
+};
+
 type Props = {
   feed: PostProps[];
 };
 
 const Blog: React.FC<Props> = (props) => {
+  const [team1Players, setTeam1Players] = useState<Player[]>(defaultPlayers());
+
+  const [team2Players, setTeam2Players] = useState<Player[]>(defaultPlayers());
+
+  const updatePlayerRuns = (
+    teamIndex: number,
+    playerIndex: number,
+    runs: number,
+    action: null | string
+  ): void => {
+    if (teamIndex === 1) {
+      const updatedPlayers = [...team1Players];
+      updatedPlayers[playerIndex].runs += runs;
+      if (action) {
+        updatedPlayers[playerIndex].allActions.push(action);
+      } else {
+        updatedPlayers[playerIndex].allActions.push(runs.toString());
+      }
+      setTeam1Players(updatedPlayers);
+    } else if (teamIndex === 2) {
+      const updatedPlayers = [...team2Players];
+      updatedPlayers[playerIndex].runs += runs;
+      if (action) {
+        updatedPlayers[playerIndex].allActions.push(action);
+      } else {
+        updatedPlayers[playerIndex].allActions.push(runs.toString());
+      }
+      setTeam2Players(updatedPlayers);
+    }
+  };
   return (
     <Layout>
       <div className="page">
         <main>
           <Board>
-            <Team name="Team 1" />
+            <Team name="Team 1" players={team1Players} />
             <Scoreboard />
-            <Team name="Team 2" />
+            <Scoring onScoreUpdate={updatePlayerRuns} />
+            <Team name="Team 2" players={team2Players} />
           </Board>
           <h2>Public Feed</h2>
           {props.feed.map((post) => (
