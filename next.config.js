3135efb78f5e6a11c5ec4ab7883b35e7bf773fa4
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-03-01 22:46:14 +0000

    move page extensions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   compiler: {
     // Enables the styled-components SWC transform
-    styledComponents: true,
-    pageExtensions: ['index.tsx', 'drafts.tsx', 'create.tsx']
-  }
+    styledComponents: true
+  },
+  pageExtensions: ['index.tsx', 'drafts.tsx', 'create.tsx']
 };
