f80cc017d2adc5c513753f1fdec21b704b2fc285
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-03-02 06:48:19 +0000

    create __tests__ folder

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ module.exports = {
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
-  },
-  pageExtensions: ['index.tsx', 'drafts.tsx', 'create.tsx']
+  }
 };
