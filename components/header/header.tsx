c3ab63c9367f2a3ce66b9397928fe7c6ed085c4d
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-04-06 10:13:09 +0100

    style title

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,16 +1,28 @@
 // Header.tsx
 import React from 'react';
 import Nav from '../nav/nav';
+import styled from '@emotion/styled';
 
 const Header: React.FC = () => {
   return (
     <>
       <header>
         <Nav />
-        <h1>20Twenty Score</h1>
+        <StyledHeading>20Twenty Score</StyledHeading>
       </header>
     </>
   );
 };
+
+const StyledHeading = styled.h1`
+  color: green;
+  text-align: center;
+  margin: 0;
+  padding: 1rem;
+  font-size: 2rem;
+  background-color: #f0f0f0;
+  border-bottom: 1px solid #ccc;
+  font-family: 'Oswald', sans-serif;
+`;
 
 export default Header;
