9c5d19b5c73bd594c46d4bde2111418deaf893be
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-04-06 11:08:40 +0100

    fic nav tests

--- components/nav/nav.spec.tsx
+++ components/nav/nav.spec.tsx
@@ -35,8 +35,6 @@ const gameScore = [
   { players: [], name: 'Team 2', index: 1, totalRuns: 0, totalWickets: 0 }
 ] as GameScore;
 const setPlayerScore = jest.fn();
-const setMostRecentAction = jest.fn();
-const mostRecentAction = { runs: 0, action: null };
 
 describe('Nav Component', () => {
   beforeEach(() => {
@@ -230,8 +228,8 @@ describe('Nav Component', () => {
 
     expect(localStorageMock.removeItem).toHaveBeenCalled();
     expect(setGameScore).toHaveBeenCalledWith([
-      { players: defaultPlayers(), name: 'Team 1', index: 0 },
-      { players: defaultPlayers(), name: 'Team 2', index: 1 }
+      { players: defaultPlayers(), name: 'Team 1', index: 0, totalRuns: 0, totalWickets: 0 },
+      { players: defaultPlayers(), name: 'Team 2', index: 1, totalRuns: 0, totalWickets: 0 }
     ]);
   });
 });
