d6b8d2c02522e7d5ce3d5e8ec84aa1ca82601256
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-04-13 11:59:48 +0100

    next auth unit tests

---
+++ pages/api/auth/[...nextauth].spec.ts
@@ -0,0 +1,101 @@
+import NextAuth from 'next-auth';
+import { authHandler } from './[...nextauth]';
+
+jest.mock('next-auth', () => jest.fn());
+
+describe('Nextauth', () => {
+  const OLD_ENV = process.env;
+
+  it('should call NextAuth with correct options', async () => {
+    process.env = {
+      ...OLD_ENV,
+      NODE_ENV: 'test',
+      GITHUB_ID: 'mock-github-id',
+      GITHUB_SECRET: 'mock-github-secret',
+      FACEBOOK_ID: 'mock-facebook-id',
+      FACEBOOK_SECRET: 'mock-facebook-secret',
+      GOOGLE_ID: 'mock-google-id',
+      GOOGLE_SECRET: 'mock-google-secret',
+      SECRET: 'mock-secret'
+    };
+    const req = {} as any;
+    const res = {} as any;
+    console.log(33, process.env);
+    await authHandler(req, res);
+    expect(NextAuth).toHaveBeenCalledWith(
+      req,
+      res,
+      expect.objectContaining({
+        providers: expect.arrayContaining([
+          expect.objectContaining({
+            id: 'github',
+            options: {
+              clientId: 'mock-github-id',
+              clientSecret: 'mock-github-secret'
+            }
+          }),
+          expect.objectContaining({
+            id: 'facebook',
+            options: {
+              clientId: 'mock-facebook-id',
+              clientSecret: 'mock-facebook-secret'
+            }
+          }),
+          expect.objectContaining({
+            id: 'google',
+            options: {
+              clientId: 'mock-google-id',
+              clientSecret: 'mock-google-secret'
+            }
+          })
+        ])
+      })
+    );
+  });
+
+  it('should call NextAuth when env vars are missing', async () => {
+    process.env = {
+      ...OLD_ENV,
+      NODE_ENV: 'test',
+      SECRET: 'mock-secret'
+    };
+    delete process.env.GITHUB_ID;
+    delete process.env.GITHUB_SECRET;
+    delete process.env.FACEBOOK_ID;
+    delete process.env.FACEBOOK_SECRET;
+    delete process.env.GOOGLE_ID;
+    delete process.env.GOOGLE_SECRET;
+    const req = {} as any;
+    const res = {} as any;
+    await authHandler(req, res);
+    expect(NextAuth).toHaveBeenCalledWith(
+      req,
+      res,
+      expect.objectContaining({
+        providers: expect.arrayContaining([
+          expect.objectContaining({
+            id: 'github',
+            options: {
+              clientId: '',
+              clientSecret: ''
+            }
+          }),
+          expect.objectContaining({
+            id: 'facebook',
+            options: {
+              clientId: '',
+              clientSecret: ''
+            }
+          }),
+          expect.objectContaining({
+            id: 'google',
+            options: {
+              clientId: '',
+              clientSecret: ''
+            }
+          })
+        ])
+      })
+    );
+  });
+});
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,24 +8,27 @@ import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import prisma from '../../../lib/prisma';
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
-
-const options = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || ''
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID || '',
-      clientSecret: process.env.FACEBOOK_SECRET || ''
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_SECRET || ''
-    })
-  ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET
+export const authHandler: NextApiHandler = (req, res) => {
+  const options = {
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID || '',
+        clientSecret: process.env.GITHUB_SECRET || ''
+      }),
+      FacebookProvider({
+        clientId: process.env.FACEBOOK_ID || '',
+        clientSecret: process.env.FACEBOOK_SECRET || ''
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID || '',
+        clientSecret: process.env.GOOGLE_SECRET || ''
+      })
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.SECRET
+  };
+  console.log(40, process.env);
+  NextAuth(req, res, options);
 };
+
+console.log(10, process.env);
